35bdfdf430abde1cd7c23fb7b3062fbefc292166
karan jain <karanjain2212@gmail.com>
2022-12-05 16:45:56 +0530

    initialize

---
+++ Module/ui.js
@@ -0,0 +1,24 @@
+export const registeredBooks = document.querySelector(".book-list");
+export const author = document.querySelector(".author");
+export const Title = document.querySelector(".title");
+
+export function addBooks(Books) {
+  registeredBooks.innerHTML = "";
+  for (let i = 0; i < Books.length; i++) {
+    registeredBooks.innerHTML += `
+      <div class="title-and-author"> 
+        <p class="Title">"${Books[i].title}" by  ${Books[i].author}</p>
+        <button class="button remove" id="${i}">Remove</button>
+      </div>
+
+     `;
+    Title.value = "";
+    author.value = "";
+    Title.focus();
+  }
+}
+export function deleteBook(el) {
+  if (el.classList.contains("remove")) {
+    el.parentElement.remove();
+  }
+}
---
+++ index.js
@@ -0,0 +1,98 @@
+"use strict";
+
+import { addBooks, deleteBook } from "./Module/ui.js";
+
+import { Title, author, registeredBooks } from "./Module/ui.js";
+import { DateTime } from "./Module/luxon.js";
+// const Title = document.querySelector(".title");
+/* eslint-disable no-unused-vars */
+// const date = new Date();
+const showDateAndTime = document.querySelector(".date-and-time");
+const ListMenu = document.querySelector(".list");
+const AddMenu = document.querySelector(".add");
+const ContactMenu = document.querySelector(".contactmenu");
+const addButton = document.querySelector(".addbook");
+// const time = date.toLocaleTimeString("en-US", {
+//   hour: "numeric",
+//   minute: "numeric",
+//   hour12: true,
+// });
+
+// showDateAndTime.innerHTML = `${date.toDateString()}, ${time}`;
+
+const setTime = () => {
+  showDateAndTime.innerHTML = DateTime.now().toLocaleString(
+    DateTime.DATETIME_MED_WITH_SECONDS
+  );
+};
+setInterval(setTime, 1000);
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+let Books = [];
+
+window.onload = () => {
+  if (registeredBooks.innerHTML === "") {
+    document.querySelector(".emptymsg").style.display = "block";
+    registeredBooks.style.border = "none";
+  } else {
+    document.querySelector(".emptymsg").style.display = "none";
+  }
+  if (localStorage.getItem("Books")) {
+    Books = JSON.parse(localStorage.getItem("Books"));
+    registeredBooks.style.border = "1px solid black";
+    document.querySelector(".emptymsg").style.display = "none";
+  }
+  addBooks(Books);
+};
+
+//add
+
+addButton.addEventListener("click", (e) => {
+  if (Title.value === "" || author.value === "") {
+    e.preventDefault();
+  } else {
+    const book = new Book(Title.value, author.value);
+    Books.push(book);
+    addBooks(Books);
+    localStorage.setItem("Books", JSON.stringify(Books));
+    document.querySelector(".emptymsg").style.display = "none";
+    registeredBooks.style.border = "1px solid black";
+  }
+});
+
+//remove
+
+document.querySelector(".booksdata").addEventListener("click", (e) => {
+  if (!localStorage.getItem("Books")) {
+    document.querySelector(".emptymsg").style.display = "block";
+    registeredBooks.style.border = "none";
+  } else {
+    Books.splice(e.target.id, 1);
+
+    localStorage.setItem("Books", JSON.stringify(Books));
+    deleteBook(e.target);
+  }
+});
+
+ListMenu.addEventListener("click", () => {
+  document.querySelector(".booksdata").style.display = "block";
+  document.querySelector(".addbooks").style.display = "none";
+  document.querySelector(".contact").style.display = "none";
+});
+AddMenu.addEventListener("click", () => {
+  document.querySelector(".booksdata").style.display = "none";
+  document.querySelector(".addbooks").style.display = "block";
+  document.querySelector(".contact").style.display = "none";
+});
+
+ContactMenu.addEventListener("click", () => {
+  document.querySelector(".booksdata").style.display = "none";
+  document.querySelector(".addbooks").style.display = "none";
+  document.querySelector(".contact").style.display = "block";
+});
